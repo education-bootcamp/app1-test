510a78f61d413141e2c6bfa653e4f8a52b74b7cf
ozen-hub <info.ozenhub@gmail.com>
2023-10-04 22:06:54 +0530

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name='';
+
+  message='this data object is from the Parent component!';
 
 }
